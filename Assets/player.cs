0b11d3b433373b4a714feecf9cf90235748b91da
jerry990831 <weiqiwang@umass.edu>
2021-10-23 18:40:50 -0400

    add attack and roll

--- Assets/player.cs
+++ Assets/player.cs
@@ -10,10 +10,19 @@ public class player : MonoBehaviour
     public string down;
     public string right;
     public string left;
+    public string isrun;
+    public float run;
+    public string roll;
+    public bool isroll;
+    public string attack;
+    public bool isattack;
+
     public float upordown;
     public float rightorleft;
     private float vu;
     private float vr;
+    private float vrun;
+  
     void Start()
     {
         
@@ -24,10 +33,16 @@ public class player : MonoBehaviour
     {
         float du;
         float dr;
-
+        float drun;
         du = Convert.ToInt32(Input.GetKey(up))-Convert.ToInt32(Input.GetKey(down));
         dr = Convert.ToInt32(Input.GetKey(right))-Convert.ToInt32(Input.GetKey(left));
+        
         upordown = Mathf.SmoothDamp(upordown,du,ref vu,0.1f);
         rightorleft = Mathf.SmoothDamp(rightorleft,dr,ref vr,0.1f);
+
+        drun = Convert.ToInt32(Input.GetKey(isrun));
+        run =  Mathf.SmoothDamp(run,drun,ref vrun,0.1f);
+        isroll = Input.GetKeyDown(roll); 
+        isattack = Input.GetKeyDown(attack);
     }
 }
